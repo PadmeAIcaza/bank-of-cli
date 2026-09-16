938b51f0759ed083c416eea594af5152c4d0db45
spicypusheen <padme.garrapata@gmail.com>
2026-09-16 09:30:35 -0500

    Initial commit

---
+++ src/main/java/com/bankofcli/bankofcli/Main.java
@@ -0,0 +1,18 @@
+package com.bankofcli.bankofcli;
+import com.bankofcli.bankofcli.util.Database;
+import java.sql.Connection;
+import java.sql.SQLException;
+public class Main {
+    public static void main(String[] args) {
+
+        try (Connection connection = Database.getConnection()) {
+
+            System.out.println("Connected to PostgreSQL successfully!");
+
+        } catch (SQLException e) {
+
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
